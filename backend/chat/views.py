aabd9e4059abd294cfaa6d527fbbff290da3c183
Jack Ma <1791670972@qq.com>
2021-03-23 09:28:45 +0800

    refactoring user view

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -9,86 +9,122 @@ from .codeSender import codeSender
 code_sender = codeSender()
 
 
-def get_login_data(user):
-    token = generate_token(user)
-    friends = [(i.id, i.nickname) for i in user.friends.all()]
-    friends += [(i.id, i.nickname) for i in user.friends_set.all()]
-    friends = list(set(friends))
-    chatroom_list = [(i.id, i.name) for i in user.chatroom.all()]
-    unread_message_db = user.unread.all()
-    unread_message = [
-        {
-            "from": (u_message.message.from_user.id, u_message.message.from_user.nickname),
-            "to": u_message.message.to,
-            "content": u_message.message.content,
-            "time": timezone.localtime(u_message.message.createTime).isoformat()
+class UserView:
+
+    @staticmethod
+    def get_login_data(user):
+        token = generate_token(user)
+        friends = [(i.id, i.nickname) for i in user.friends.all()]
+        friends += [(i.id, i.nickname) for i in user.friends_set.all()]
+        friends = list(set(friends))
+        chatroom_list = [(i.id, i.name) for i in user.chatroom.all()]
+        unread_message_db = user.unread.all()
+        unread_message = [
+            {
+                "from": (u_message.message.from_user.id, u_message.message.from_user.nickname),
+                "to": u_message.message.to,
+                "content": u_message.message.content,
+                "time": timezone.localtime(u_message.message.createTime).isoformat()
+            }
+            for u_message in unread_message_db
+        ]
+        unread_message_db.delete()
+        return {
+            'self': {
+                'id': user.id,
+                'nickname': user.nickname
+            },
+            "token": token.content,
+            "friends": friends,
+            "chatroom_list": chatroom_list,
+            "unread_message": unread_message
         }
-        for u_message in unread_message_db
-    ]
-    unread_message_db.delete()
-    return {
-        'self': {
-            'id': user.id,
-            'nickname': user.nickname
-        },
-        "token": token.content,
-        "friends": friends,
-        "chatroom_list": chatroom_list,
-        "unread_message": unread_message
-    }
 
+    @staticmethod
+    @csrf_exempt
+    def register(request):
+        if request.method == "POST":
+            data = json.loads(request.body)
+            email = data.get("email")
+            nickname = data.get("nickname")
+            password = data.get("password")
+            try:
+                User.objects.get(email=email)
+            except User.DoesNotExist:
+                user_id = get_user_id()
+                user = User(id=user_id, email=email, nickname=nickname, password=parse_password(password))
+                user.save()
+                return wrap_response("", user_id)
+            return wrap_response("该邮箱已注册")
+        else:
+            return wrap_response("请求方法错误")
 
-@csrf_exempt
-def register(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        email = data.get("email")
-        nickname = data.get("nickname")
-        password = data.get("password")
+    @staticmethod
+    @csrf_exempt
+    def login(request):
+        if request.method == "POST":
+            data = json.loads(request.body)
+            identity = data.get("identity")
+            password = data.get("password")
+            try:
+                # 邮箱登录
+                if "@" in identity:
+                    user = User.objects.get(email=identity)
+                else:
+                    user = User.objects.get(id=identity)
+                if user.password == parse_password(password):
+                    login_data = UserView.get_login_data(user)
+                    return wrap_response("", login_data)
+                else:
+                    return wrap_response("密码错误")
+            except User.DoesNotExist:
+                return wrap_response("用户不存在")
+        else:
+            return wrap_response('wrong method')
+
+    @staticmethod
+    def get_userinfo(request, userid):
         try:
-            User.objects.get(email=email)
+            user = User.objects.get(id=userid)
         except User.DoesNotExist:
-            user_id = get_user_id()
-            user = User(id=user_id, email=email, nickname=nickname, password=parse_password(password))
-            user.save()
-            return wrap_response("", user_id)
-        return wrap_response("该邮箱已注册")
-    else:
-        return wrap_response("请求方法错误")
+            return wrap_response("wrong id")
+        return wrap_response("", {
+            "id": user.id,
+            "nickname": user.nickname,
+            "email": user.email
+        })
 
-
-@csrf_exempt
-def login(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        identity = data.get("identity")
-        password = data.get("password")
+    @staticmethod
+    def send_code(request):
+        email = request.GET.get('email')
+        ty = int(request.GET.get('type'))  # 发送验证码类型，1-注册，2-重置密码
+        content = generate_code()
+        now = timezone.now()
+        if ty == 1:
+            try:
+                User.objects.get(email=email)
+                return wrap_response('该邮箱已注册')
+            except User.DoesNotExist:
+                pass
+        elif ty == 2:
+            try:
+                User.objects.get(email=email)
+            except User.DoesNotExist:
+                return wrap_response('该邮箱未注册')
+        else:
+            return wrap_response('发送类型错误')
         try:
-            if "@" in identity:
-                user = User.objects.get(email=identity)
+            ver_code = VerCode.objects.get(email=email)
+            if (timezone.now() - ver_code.time).total_seconds() <= 60:
+                return wrap_response('发送间隔太短')
             else:
-                user = User.objects.get(id=identity)
-            if user.password == parse_password(password):
-                login_data = get_login_data(user)
-                return wrap_response("", login_data)
-            else:
-                return wrap_response("密码错误")
-        except User.DoesNotExist:
-            return wrap_response("用户不存在")
-    else:
-        return wrap_response('wrong method')
-
-
-def get_userinfo(request, userid):
-    try:
-        user = User.objects.get(id=userid)
-    except User.DoesNotExist:
-        return wrap_response("wrong id")
-    return wrap_response("", {
-        "id": user.id,
-        "nickname": user.nickname,
-        "email": user.email
-    })
+                ver_code.content = content
+                ver_code.time = now
+        except VerCode.DoesNotExist:
+            ver_code = VerCode(email=email, content=content, time=now)
+        ver_code.save()
+        code_sender.send(email, content)
+        return wrap_response('')
 
 
 @csrf_exempt
@@ -169,35 +205,3 @@ def get_message(request, user):
     return wrap_response('', {
         'message_list': message_list
     })
-
-
-def send_code(request):
-    email = request.GET.get('email')
-    ty = int(request.GET.get('type'))   # 发送验证码类型，1-注册，2-重置密码
-    content = generate_code()
-    now = timezone.now()
-    if ty == 1:
-        try:
-            User.objects.get(email=email)
-            return wrap_response('该邮箱已注册')
-        except User.DoesNotExist:
-            pass
-    elif ty == 2:
-        try:
-            User.objects.get(email=email)
-        except User.DoesNotExist:
-            return wrap_response('该邮箱未注册')
-    else:
-        return wrap_response('发送类型错误')
-    try:
-        ver_code = VerCode.objects.get(email=email)
-        if (timezone.now() - ver_code.time).total_seconds() <= 60:
-            return wrap_response('发送间隔太短')
-        else:
-            ver_code.content = content
-            ver_code.time = now
-    except VerCode.DoesNotExist:
-        ver_code = VerCode(email=email, content=content, time=now)
-    ver_code.save()
-    code_sender.send(email, content)
-    return wrap_response('')
