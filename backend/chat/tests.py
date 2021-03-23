13a3d28be2a812eee01b7f4465e9c31f992f3c5a
Jack Ma <1791670972@qq.com>
2021-03-23 09:28:56 +0800

    refactoring user test

--- backend/chat/tests.py
+++ backend/chat/tests.py
@@ -1,35 +1,49 @@
 from django.test import TestCase, Client
 from chat.views import *
-from django.http import HttpRequest
-import json
 
 
-class AuthTest(TestCase):
+class UserTest(TestCase):
 
     def setUp(self) -> None:
         self.client = Client()
+        self.user_id = ''
+        self.login_data = ''
 
-    def test_register_and_login(self):
+    def login(self, use_id=False):
+        login_data = {
+            "identity": "1791670972@qq.com",
+            "password": "12138"
+        }
+        if use_id:
+            login_data['identity'] = str(self.user_id)
+        response = self.client.post('/users/auth', login_data, content_type='application/json')
+        self.assertEqual(len(response.json()['data']['token']), 64)
+        self.login_data = response.json()['data']
+
+    def register_and_login(self):
         """测试注册之后登录"""
         register_data = {
             "email": "1791670972@qq.com",
             "nickname": "楷禅",
             "password": "12138"
         }
-        login_data = {
-            "identity": "1791670972@qq.com",
-            "password": "12138"
-        }
+
         response = self.client.post('/users/', register_data, content_type='application/json')
-        user_id = response.json().get('data')
+        self.user_id = response.json().get('data')
         # 注册成功之后用户id应该在10000~99999之间
-        self.assertIn(user_id, range(10000, 100000))
+        self.assertIn(self.user_id, range(10000, 100000))
 
         # 测试邮箱登录
-        response = self.client.post('/users/auth', login_data, content_type='application/json')
-        self.assertEqual(len(response.json()['data']['token']), 64)
+        self.login()
 
         # 测试账号登录
-        login_data['identity'] = str(user_id)
-        response = self.client.post('/users/auth', login_data, content_type='application/json')
-        self.assertEqual(len(response.json()['data']['token']), 64)
+        self.login(use_id=True)
+
+    def get_userinfo(self):
+        """"""
+        response = self.client.get(f'/users/{self.user_id}')
+        self.assertEqual(response.json()['data']['email'], '1791670972@qq.com')
+
+    def test_user(self):
+        self.register_and_login()
+        self.get_userinfo()
