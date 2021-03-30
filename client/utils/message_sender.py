22cd604a3329e418e193a0cad495301165b917a3
ArmandAlbert <1286313960@qq.com>
2021-03-30 21:25:37 +0800

    修复可能有的连接错误

--- client/utils/message_sender.py
+++ client/utils/message_sender.py
@@ -18,10 +18,13 @@ class MessageSender(Thread):
     def run(self) -> None:
         while True:
             if global_data.client is not None:
-                to, content = global_data.message_sender_queue.get()
-                # 阻塞获取消息完成之后连接可能断掉
-                if global_data.client is None:
+                try:
+                    to, content = global_data.message_sender_queue.get()
+                    # 阻塞获取消息完成之后连接可能断掉
+                    if global_data.client is None:
+                        global_data.client = None
+                        continue
+                    global_data.client.sendall(self.__wrap_message(to, content))
+                    global_data.logger.info(f'向{to}发送消息成功')
+                except Exception:
                     global_data.client = None
-                    continue
-                global_data.client.sendall(self.__wrap_message(to, content))
-                global_data.logger.info(f'向{to}发送消息成功')
