900cf69fb5c674693043271ba5eeee252839d5f0
Jack Ma <1791670972@qq.com>
2021-04-04 12:34:26 +0800

    更换提示方案

--- client/utils/toaster_sender.py
+++ client/utils/toaster_sender.py
@@ -3,6 +3,9 @@ from win10toast import ToastNotifier
 from globalFile import GlobalData
 from utils.notice_sender import NotificationWindow
 
+import plyer.platforms.win.notification
+from plyer import notification
+
 toaster = ToastNotifier()
 global_data = GlobalData()
 
@@ -14,7 +17,8 @@ class ToasterSender(Thread):
     def run(self):
         # 清空消息队列
         while True:
-            toaster.show_toast('消息提示', global_data.toast_message_queue.get())
+            notification.notify('消息提示', global_data.toast_message_queue.get(), app_name='SQQ', timeout=0.5,
+                                app_icon='imgs/chat.ico')
             # 使用自定义消息弹窗，但是会出现QBasicTimer无法对应的问题。
             # 可以通过传递父窗体对象来解决问题
             # NotificationWindow.info("消息提示", global_data.toast_message_queue.get())
