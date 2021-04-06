aa3b6967166dc8c5a4ff83ad2ae7060cf7bc0c96
ArmandAlbert <1286313960@qq.com>
2021-04-06 18:44:07 +0800

    优化语音通话界面

--- client/voice_call/callVoiceCallWindow.py
+++ client/voice_call/callVoiceCallWindow.py
@@ -1,15 +1,10 @@
-from datetime import datetime
-import json
 import sys
 import time
-import os
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeWidgetItem, QWidget, QHBoxLayout, QLabel, QSpacerItem, \
-    QSizePolicy, QTreeWidget, QMessageBox, QMenu, QAction, QFileDialog
-from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtCore import Qt, pyqtSignal, QPropertyAnimation, QThread
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from globalFile import GlobalData
 from voice_call.voiceCall import Ui_Form
+from PyQt5 import QtGui
+from PyQt5.QtCore import QTimer
 
 global_data = GlobalData()
 
@@ -19,7 +14,35 @@ class VoicePhoneWindow(QMainWindow, Ui_Form):
     def __init__(self, loginInfo):
         super(VoicePhoneWindow, self).__init__()
         self.setupUi(self)
+        self.setWindowTitle("语音通话")
+        self.setWindowIcon(QtGui.QIcon('imgs/phone.png'))
+        self.setStyleSheet('''
+        QPushButton{
+            border-radius: 50px;
+            border-style: outset;
+            background: red;
+            border-image: url(imgs/phone.png);
+            }
+        QPushButton:hover{
+            border-radius: 50px;
+            border-style: outset;
+            background-color: #ff5964;
+            border-image: url(imgs/phone.png);
+            }
+        QWidget#Form{
+            background-color: lightgrey;
+            }
+        ''')
         self.loginInfo = loginInfo
+        self.timer = QTimer()  # 初始化定时器
+        self.timer.timeout.connect(self.time)
+
+    def time(self):
+        self.label_2.setText('{} s'.format(time.time()))
+
+    # 开始语音通话
+    def startVoicePhone(self):
+        self.timer.start(2 * 1000)
 
 
 if __name__ == '__main__':
--- client/voice_call/voiceCall.py
+++ client/voice_call/voiceCall.py
@@ -11,26 +11,38 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(310, 492)
+        Form.resize(351, 570)
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(50, 40, 210, 210))
+        self.label.setGeometry(QtCore.QRect(100, 60, 151, 151))
         font = QtGui.QFont()
         font.setPointSize(70)
         self.label.setFont(font)
         self.label.setStyleSheet("border-color: lightyellow;background-color: #FFD700;\n"
-"border-radius:100%;\n"
+"border-radius:50%;\n"
 "color: black;\n"
 "")
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(100, 330, 111, 111))
-        self.pushButton.setStyleSheet("border-radius: 50px;\n"
-"border-style: outset;\n"
-"background-color: red;\n"
-"border-image: url(imgs/phone.png)")
+        self.pushButton.setGeometry(QtCore.QRect(120, 350, 111, 111))
+        self.pushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.pushButton.setStyleSheet("")
         self.pushButton.setText("")
         self.pushButton.setObjectName("pushButton")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(130, 270, 91, 21))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(130, 460, 91, 51))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.label_3.setFont(font)
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -39,4 +51,6 @@ class Ui_Form(object):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.label.setText(_translate("Form", "韩"))
+        self.label_2.setText(_translate("Form", "00:00"))
+        self.label_3.setText(_translate("Form", "挂断"))
 
